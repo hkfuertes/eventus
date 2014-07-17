aacef92d9d948b384af787d44e298f7af53f9df6
Miguel Fuertes <hkfuertes@miguels-mbp.home>
2014-07-17 18:09:39 +0200

    Para que funcione

--- proyecto/apps/api/modules/users/actions/actions.class.php
+++ proyecto/apps/api/modules/users/actions/actions.class.php
@@ -8,21 +8,19 @@
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class usersActions extends sfActions
-{
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex(sfWebRequest $request)
-  {
-    //$this->forward('default', 'module');
-  }
-  
-  public function executeGetUsers(sfWebRequest $request){
-      $users = sfGuardUserPeer::doSelect(new Criteria());
-      print_r(json_encode($users)); die();
-      return $this->renderText(json_encode($users));
-  }
+class usersActions extends sfActions {
+
+    public function executeIndex(sfWebRequest $request) {
+        $this->forward('default', 'index');
+    }
+
+    /**
+     * Testing only: Returns an array of users with their name.
+     */
+    public function executeGetUsers(sfWebRequest $request) {
+        $users = sfGuardUserPeer::doSelect(new Criteria());
+        foreach($users as $user) $retU[] = $user->getUsername();
+        return $this->renderText(json_encode($retU));
+    }
+
 }
